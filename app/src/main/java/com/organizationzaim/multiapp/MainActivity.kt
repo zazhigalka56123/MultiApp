0fcc1fbb60490960238510c61159c6055dfe4c00
Zazhigalka56123 <61152519+zazhigalka56123@users.noreply.github.com>
2020-10-26 21:45:52 +0500

    done WebActivity.kt on 50% percent

--- app/src/main/java/com/organizationzaim/multiapp/MainActivity.kt
+++ app/src/main/java/com/organizationzaim/multiapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.organizationzaim.multiapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
